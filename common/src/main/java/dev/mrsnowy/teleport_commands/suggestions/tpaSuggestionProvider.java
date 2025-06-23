847184e921d8a0c3b79486dd31e857e198fdc74c
steffengrimm <steffen.grimm@reservision.de>
2025-06-23 22:57:47 +0000

    replaced all ArrayLists by HashMaps in otder to avoid linear searches

--- common/src/main/java/dev/mrsnowy/teleport_commands/suggestions/tpaSuggestionProvider.java
+++ common/src/main/java/dev/mrsnowy/teleport_commands/suggestions/tpaSuggestionProvider.java
@@ -13,25 +13,24 @@ import net.minecraft.server.level.ServerPlayer;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
-
 public class tpaSuggestionProvider implements SuggestionProvider<CommandSourceStack> {
     @Override
-    public CompletableFuture<Suggestions> getSuggestions(CommandContext<CommandSourceStack> context, SuggestionsBuilder builder) {
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<CommandSourceStack> context,
+            SuggestionsBuilder builder) {
         try {
             ServerPlayer player = context.getSource().getPlayerOrException();
 
-            List<tpa.tpaArrayClass> playerTpaList = tpa.tpaList.stream()
-                    .filter(tpa -> Objects.equals(player.getStringUUID(), tpa.RecPlayer))
-                    .toList();
-
-            for (tpa.tpaArrayClass tpaEntry : playerTpaList) {
-
-                Optional<String> recPlayerName = Optional.ofNullable(context.getSource().getServer().getPlayerList().getPlayer(UUID.fromString(tpaEntry.InitPlayer))).map(p -> p.getName().getString());
-
-                if (recPlayerName.isPresent()) {
-                    builder.suggest(recPlayerName.get());
-                }
-            }
+            tpa.tpaRequestMap.keySet().stream()
+                    .filter(pair -> Objects.equals(player.getStringUUID(), pair.getValue()))
+                    .forEach(pair -> {
+                        String initPlayer = pair.getKey();
+                        Optional<String> recPlayerName = Optional.ofNullable(
+                                context.getSource().getServer().getPlayerList().getPlayer(UUID.fromString(initPlayer)))
+                                .map(p -> p.getName().getString());
+                        if (recPlayerName.isPresent()) {
+                            builder.suggest(recPlayerName.get());
+                        }
+                    });
 
             // Build and return the suggestions
             return builder.buildFuture();
